d4fa5d5089190ffa8fe020da062fdc924b9290d7
Pooya Shams <pooya.shams.k@gmail.com>
2020-01-05 21:58:57 +0330

    massspring initial commition

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -- In the name of god --
+# Project : massspring (mass+spring)
+# Author : Pooya.Sh.K 
+# Inspired by Saeed Sarkarati
+
+"""
+massspring
+
+using this library you can create great
+simulations of most of physical environments
+which can be calculated presizely.
+you can create many types of objects from which
+"mass" and "spring" are main ones.
+"""
+
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = '1'
+import pygame, sys, warnings
+from math import *
