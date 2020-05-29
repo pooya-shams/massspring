e9d48dc38629094031b7bdf48ed93bed48a4a09a
Pooya Shams kolahi <pooya.shams.k@gmail.com>
2020-05-29 14:36:19 +0430

    Updated example 5 to better demonstrate the collisions

--- massspring/example 5 (collision).py
+++ massspring/example 5 (collision).py
@@ -1,10 +1,10 @@
 import massspring as m
 
-m1 = m.mass(m=20, r=20, x=-100, y=0, z=0, vx=20, vy=0, vz=0,
+m1 = m.mass(m=20, r=45, x=-50, y=5, z=0, vx=20, vy=0, vz=0,
             moveable=True, solid=True, bound=True,
             color=(255, 0, 0), visible=True)
-m2 = m.mass(m=20, r=20, x=100, y=0, z=0, vx=-20, vy=0, vz=0,
+m2 = m.mass(m=30, r=60, x=50, y=0, z=5, vx=-20, vy=0, vz=0,
             moveable=True, solid=True, bound=True,
             color=(0, 255, 0), visible=True)
 
-m.mainloop(1, 0)
+m.mainloop(100, 0)
