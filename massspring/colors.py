1e44a61c70d136f7f37a4971e4f7fe858b7a6afe
Pooya Shams kolahi <pooya.shams.k@gmail.com>
2020-09-04 13:51:23 +0430

    Formatted colors.py

--- massspring/colors.py
+++ massspring/colors.py
@@ -1,7 +1,10 @@
 # colors.py
-# this file contains a number of RGB colors in tuple format
-# which will be used to identify the color of objects in the
-# pygame surfaces.
+
+"""
+this file contains a number of RGB colors in tuple format
+which will be used to identify the color of objects in the
+pygame surfaces.
+"""
 
 # colors [= ( R ,  G ,  B )]
 WHITE = (255, 255, 255)
@@ -12,23 +15,23 @@ GREY = (128, 128, 128)
 GRAY = GREY
 BLUE_GREY = (94, 124, 139)
 BLUE_GRAY = BLUE_GREY
-BLACK = (0,   0,   0)
-DARK_RED = (128,   0,   0)
-PURE_RED = (255,   0,   0)
-RED = (246,  64,  44)
-PINK = (235,  19,  96)
-PURE_PINK = (255,   0, 128)
-ROSE = (246,  39, 157)
-MAGENTA = (255,   0, 255)
+BLACK = (0, 0, 0)
+DARK_RED = (128, 0, 0)
+PURE_RED = (255, 0, 0)
+RED = (246, 64, 44)
+PINK = (235, 19, 96)
+PURE_PINK = (255, 0, 128)
+ROSE = (246, 39, 157)
+MAGENTA = (255, 0, 255)
 VIOLET = (238, 130, 239)
-PURPLE = (156,  26, 177)
-PURE_PURPLE = (128,   0, 128)
-DARK_PURPLE = (102,  51, 185)
-INDEGO = (61,  76, 183)
-DARK_BLUE = (0,   0, 128)
+PURPLE = (156, 26, 177)
+PURE_PURPLE = (128, 0, 128)
+DARK_PURPLE = (102, 51, 185)
+INDEGO = (61, 76, 183)
+DARK_BLUE = (0, 0, 128)
 NAVY = DARK_BLUE
 BLUE = (15, 147, 245)
-PURE_BLUE = (0,   0, 255)
+PURE_BLUE = (0, 0, 255)
 WATER = (0, 166, 246)
 SKY = (140, 220, 250)
 PURE_CYAN = (0, 255, 255)
@@ -36,22 +39,22 @@ AQUA = PURE_CYAN
 CYAN = (0, 187, 213)
 TEAL = (0, 149, 135)
 DARK_CYAN = (0, 128, 128)
-DARK_GREEN = (0, 128,   0)
-GREEN = (70, 175,  74)
-GRASS = (136, 196,  64)
-PURE_GREEN = (0, 255,   0)
-OLIVE = (128, 128,   0)
-LIME = (204, 221,  30)
-YELLOW = (255, 236,  22)
-PURE_YELLOW = (255, 255,   0)
-GOLD = (252, 214,   3)
-PURE_ORANGE = (255, 192,   0)
+DARK_GREEN = (0, 128, 0)
+GREEN = (70, 175, 74)
+GRASS = (136, 196, 64)
+PURE_GREEN = (0, 255, 0)
+OLIVE = (128, 128, 0)
+LIME = (204, 221, 30)
+YELLOW = (255, 236, 22)
+PURE_YELLOW = (255, 255, 0)
+GOLD = (252, 214, 3)
+PURE_ORANGE = (255, 192, 0)
 AMBER = PURE_ORANGE
-ORANGE = (255, 151,   0)
-DARK_ORANGE = (255,  85,   3)
-BROWN = (121,  84,  70)
-LIGHT_CHOCOLATE = (133,  85,  56)
-PURE_BROWN = (128,  64,   0)
-CHOCOLATE = (105,  58,  42)
-DARK_CHOCOLATE = (72,  51,  50)
+ORANGE = (255, 151, 0)
+DARK_ORANGE = (255, 85, 3)
+BROWN = (121, 84, 70)
+LIGHT_CHOCOLATE = (133, 85, 56)
+PURE_BROWN = (128, 64, 0)
+CHOCOLATE = (105, 58, 42)
+DARK_CHOCOLATE = (72, 51, 50)
 WHITE_CHOCOLATE = (234, 225, 201)
